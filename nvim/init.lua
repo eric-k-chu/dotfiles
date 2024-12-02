e66dc7dda2ed903fcf9033542fe2ded3fe52f1b3
Eric Chu <Kiyuen88ec@gmail.com>
2024-12-01 22:09:44 -0800

    Create init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+vim.o.relativenumber = true
+
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
+
+vim.opt.fillchars = {eob = " "}
