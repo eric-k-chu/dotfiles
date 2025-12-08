43001f2f892ccbbcdfb968512a063a82a233d75e
Eric Chu <eric@terros.com>
2025-12-07 19:52:00 -0800

    sync

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,20 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.softtabstop = 2
 vim.g.mapleader = " "
-vim.g.maplocalleader = " "
 
-vim.o.number = true
-vim.o.relativenumber = true
+vim.pack.add({
+  { src = "https://github.com/nvim-telescope/telescope.nvim" },
+	{ src = "https://github.com/nvim-lua/plenary.nvim" },
+	{ src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" }
+})
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
