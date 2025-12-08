f150a684771d6d6474e724dda93e39ff1b60888e
Eric Chu <eric@terros.com>
2025-12-08 09:52:52 -0800

    sync

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,7 +9,9 @@ vim.g.mapleader = " "
 vim.pack.add({
   { src = "https://github.com/nvim-telescope/telescope.nvim" },
 	{ src = "https://github.com/nvim-lua/plenary.nvim" },
-	{ src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" }
+	{ src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" },
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/catppuccin/nvim" }
 })
 
 local builtin = require('telescope.builtin')
@@ -18,3 +20,9 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
+
+require("catppuccin").setup({
+  transparent_background = true
+})
+
+vim.cmd.colorscheme "catppuccin"
